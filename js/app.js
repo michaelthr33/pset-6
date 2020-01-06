5fa41124f0d3b74c0a8fd9e3d740fa889dfa37ad
michaelthr33 <MStaunton@ucvts.tec.nj.us>
2020-01-06 10:47:15 -0500

    made arrays

--- js/app.js
+++ js/app.js
@@ -1,21 +1,28 @@
 window.onload=function(){
-document.getElementById("button").onclick=runButton;
+  document.getElementById("button").onclick=runButton;
 
 let a="";
 let b="";
 let c="";
 let d="";
 let e="";
-let g="";
+let f="";
+let first=[]
+let second=[]
+let third=[]
+let fourth=[]
+let fifth=[]
+}
+
 
 const runButton= function(){
 
       var todo = prompt("Enter an item to add to your list.");
-      if (a=""){console.log(todo)}
-      else if (b="") {console.log(todo)}
-      else if (c="") {console.log(todo)}
-      else if (d="") {console.log(todo)}
-      else if (e="") {console.log(todo)}
-      else if (g="") {console.log(todo)}
+      if (a=""){a=todo,first=[imageonea,a,imagetwoa,imagethreea]}
+      else if (b="") {b=todo,second=[imageoneb,b,imagetwob,imagethreeb]}
+      else if (c="") {c=todo,third=[imageonec,c,imagetwoc,imagethreec]}
+      else if (d="") {d=todo,fourth=[imageoned,d,imagetwod,imagethreed]}
+      else if (e="") {e=todo,fifth=[imageonee,e,imagetwoe,imagethreee]}
+      else if (f="") {f=todo,sixth=[imageonef,f,imagetwof,imagethreef]}
+
 };
-}
