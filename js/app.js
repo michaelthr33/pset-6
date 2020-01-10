dc8eb93a672f84c3f7dc63cc3b90e09184688fb6
M. Staunton <michael@hilltopassociates.com>
2020-01-09 22:42:59 -0600

    finished

--- js/app.js
+++ js/app.js
@@ -1,128 +1,301 @@
 let elements = []
+
 var add = document.getElementById("add_button");
+
 var row = document.getElementsByClassName("row");
-let priority = document.getElementsByClassName("priority");
-let finish = document.getElementsByClassName("finish");
-let delete = document.getElementsByClassName("delete");
-let item = document.getElementsByClassName("to_do")
-let important;
+
+let importancebutton = document.getElementsByClassName("importancebutton");
+
+let resolvedbutton = document.getElementsByClassName("resolvedbutton");
+
+let deletebutton = document.getElementsByClassName("deletebutton");
+
+let list = document.getElementsByClassName("to_do")
+
+
+
+let element_prioritize;
+
+
+
 window.onload = function() {
+
   document.getElementById("table").onmouseover = startup;
+
 }
-const create = function() {
+
+
+
+const create_item = function() {
+
   let input = document.getElementById("input_item").value;
+
   if (input === "") {
+
+
+
   }
+
   else {
+
       let to_do = {
-        task: input,
+
+          task: input,
+
           priority: false,
+
           complete: false,
+
           html_row: null,
-          html_priority: null,
+
+          html_importancebutton: null,
+
           html_text: null,
-          html_delete: null
+
+          html_remove_button: null
+
       }
+
+
+
       elements.push(to_do);
+
       let index = elements.indexOf(to_do);
+
+
+
       elements[index].htmlRow = document.createElement("tr");
+
       elements[index].htmlRow.setAttribute("class", "row");
+
       document.getElementById("table").append(elements[index].htmlRow);
+
+
+
       elements[index].htmlPriorityButton = document.createElement("td");
-      elements[index].htmlPriorityButton.setAttribute("class", "priority");
+
+      elements[index].htmlPriorityButton.setAttribute("class", "importancebutton");
+
       elements[index].htmlPriorityButton.innerHTML = "!";
+
+
+
       row[index].append(elements[index].htmlPriorityButton);
+
+
+
       elements[index].htmlText = document.createElement("td");
+
       elements[index].htmlText.innerHTML = elements[index].task;
+
       elements[index].htmlText.setAttribute("class", "to_do");
+
+
+
       row[index].append(elements[index].htmlText);
+
+
+
       elements[index].htmlCompleteButton = document.createElement("td");
+
       elements[index].htmlCompleteButton.innerHTML = "&#x2713;";
-      elements[index].htmlCompleteButton.setAttribute("class", "finish");
+
+      elements[index].htmlCompleteButton.setAttribute("class", "resolvedbutton");
+
+
+
       row[index].append(elements[index].htmlCompleteButton);
+
+
+
       elements[index].htmlRemoveButton = document.createElement("td");
-      elements[index].htmlRemoveButton.setAttribute("class", "delete");
+
+      elements[index].htmlRemoveButton.setAttribute("class", "deletebutton");
+
       elements[index].htmlRemoveButton.innerHTML = "X";
+
+
+
       row[index].append(elements[index].htmlRemoveButton);
+
     }
+
     document.getElementById("input_item").value = "";
+
 };
-const remove = function() {
+
+
+
+const remove_item = function() {
+
   var removed = false;
-  for (let i = 0; i < delete.length; i++) {
-    delete[i].onclick = function() {
+
+  for (let i = 0; i < deletebutton.length; i++) {
+
+    deletebutton[i].onclick = function() {
+
         removed = true;
+
         let remove_element = row[i];
+
         remove_element.remove();
+
         elements.splice(i, 1);
+
     };
+
     if (removed) {
+
         break;
+
     }
+
   }
+
 }
-const finished = function() {
+
+
+
+const finish_item = function() {
+
   var finish = false;
-  for (let x = 0; x < finish.length; x++) {
-    finish[x].onclick = function() {
+
+  for (let x = 0; x < resolvedbutton.length; x++) {
+
+    resolvedbutton[x].onclick = function() {
+
        if (elements[x].complete == false) {
+
          finish = true;
-         item[x].style.setProperty("text-decoration", "line-through");
-         item[x].style.backgroundColor = "#baff66";
-         finish[x].style.backgroundColor = "#baff66";
+
+         list[x].style.setProperty("text-decoration", "line-through");
+
+         list[x].style.backgroundColor = "#baff66";
+
+         resolvedbutton[x].style.backgroundColor = "#baff66";
+
          elements[x].complete = true;
+
        }
+
        else if (elements[x].complete == true) {
-         finish[x].style.backgroundColor = "white";
-         item[x].style.setProperty("text-decoration", "none");
-         item[x].style.backgroundColor = "white";
+
+         resolvedbutton[x].style.backgroundColor = "white";
+
+         list[x].style.setProperty("text-decoration", "none");
+
+         list[x].style.backgroundColor = "white";
+
          elements[x].complete = false;
+
        }
+
      };
+
      if (finish) {
+
        break;
+
      }
+
   }
+
 }
-const prioritizeitem = function() {
+
+
+
+const prioritize_item = function() {
+
   var prioritize = false;
-for (let z = 0; z < priority.length; z++) {
-    priority[z].onclick = function () {
+
+  for (let z = 0; z < importancebutton.length; z++) {
+
+    importancebutton[z].onclick = function () {
+
       if (elements[z].priority == false) {
-        important = row[z]
+
+        element_prioritize = row[z]
+
         prioritize = true;
-        priority[z].style.backgroundColor = "yellow";
-        row[0].before(important);
+
+        importancebutton[z].style.backgroundColor = "blue";
+
+        row[0].before(element_prioritize);
+
         elements[z].priority = true;
+
+
+
         const objectToMove = elements[z];
+
+
+
         elements.splice(z, 1);
+
         elements.unshift(objectToMove);
+
         prioritize = true;
+
       }
+
       else if (elements[z].priority) {
-        important = row[z]
-        priority[z].style.backgroundColor = "white";
-        row[elements.length - 1].after(important);
+
+        element_prioritize = row[z]
+
+        importancebutton[z].style.backgroundColor = "white";
+
+        row[elements.length - 1].after(element_prioritize);
+
         elements[z].priority = false;
+
+
+
         let element_move = elements[z];
+
         elements.splice(z, 1);
+
         elements.push(element_move);
+
         prioritize = true;
+
       }
+
     };
+
     if (prioritize) {
+
       break;
+
     }
+
   }
+
 }
+
+
+
 const startup = function() {
-  remove();
-  finished();
-  prioritizeitem();
+
+  remove_item();
+
+  finish_item();
+
+  prioritize_item();
+
 }
-add.onclick = create
+
+
+
+add.onclick = create_item
+
+
+
 document.getElementById("input_item").addEventListener("keyup", function(event) {
+
   if (event.keyCode === 13) {
+
     document.getElementById("add_button").click();
+
   }
+
 });
